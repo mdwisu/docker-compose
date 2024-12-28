6956016a61d449990bdee67402d128babbeef691
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-28 16:23:25 +0700

    chore: docker compose: create, start, ps, stop, down, ls

---
+++ example/script.sh
@@ -0,0 +1,13 @@
+docker compose create
+docker compose create --remove-orphans
+
+# menjalankan
+docker compose start
+# melihat container
+docker compose ps -a
+# menghentikan container
+docker compose stop
+# menghapus container
+docker compose down
+# melihat daftar project
+docker compose ls
